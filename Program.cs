ba7c85deace5d7c9c2048044854ef3aaf3a809cd
Zyf <flzzyf@sina.com>
2021-01-11 18:01:36 +0800

    1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace CardExchangeServer {
     //客户端信息
@@ -89,13 +90,48 @@ namespace CardExchangeServer {
         }
 
         private void Update() {
+            //接收客户端
+            UpdateAcceptClient();
+
             //更新客户端剩余时间
             UpdateClientLifeTime();
 
             //接收信息
             UpdateReceiveMsg();
+
+            Console.SetCursorPosition(0, 0);
+            //清屏
+            for (int i = 0; i < currentLineCount; i++) {
+                Console.WriteLine("".PadRight(50));
+            }
+            currentLineCount = 0;
+
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.Green;
+            //输出服务器信息
+            if (clientInfoDic.Count > 0) {
+                foreach (var info in clientInfoDic.Values) {
+                    string str = string.Format("{0}    {1}    剩余时间:{2}", GetIP(info.socket), "连接中", info.lifeTime);
+                    Console.WriteLine(str.PadRight(50));
+
+                    currentLineCount++;
+                }
+                Console.WriteLine("=======================================".PadRight(50));
+                currentLineCount++;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //输出文本
+            foreach (var item in stringList) {
+                Console.ForegroundColor = item.color;
+                Console.WriteLine(item.msg.PadRight(50));
+                Console.ForegroundColor = ConsoleColor.White;
+
+                currentLineCount++;
+            }
         }
 
+        int currentLineCount;
 
         #region 服务器相关
 
@@ -107,34 +143,22 @@ namespace CardExchangeServer {
             server.Bind(iep);
             server.Listen(0);
 
-            Console.WriteLine(string.Format("服务器启动! (IP: {0})", GetLocalIPv4()));
-
-            //开始监听
-            server.BeginAccept(AcceptCallback, server);
+            Print(string.Format("服务器启动! (IP: {0})", GetLocalIPv4()), ConsoleColor.Yellow);
         }
 
         //接收客户端
-        void AcceptCallback(IAsyncResult ar) {
-            try {
-                Socket server = (Socket)ar.AsyncState;
-                Socket client = server.EndAccept(ar);
-
-                Console.WriteLine(string.Format(string.Format("{0}已加入", GetIP(client))));
+        void UpdateAcceptClient() {
+            if (server.Poll(0, SelectMode.SelectRead)) {
+                Socket client = server.Accept();
 
                 OnReceiveClient(client);
-
-                //继续监听
-                server.BeginAccept(AcceptCallback, server);
-
-            } catch (Exception e) {
-                Console.WriteLine(e);
             }
         }
 
         //接收信息
         void UpdateReceiveMsg() {
-            foreach (var info in clientInfoDic) {
-                Socket client = info.Value.socket;
+            foreach (var info in clientInfoDic.Values) {
+                Socket client = info.socket;
 
                 if (client == null)
                     return;
@@ -148,17 +172,17 @@ namespace CardExchangeServer {
 
                     //收到信息小于等于0，代表客户端关闭
                     if (count <= 0) {
-                        clientInfoDic.Remove(info.Key);
-                        info.Value.socket.Close();
+                        clientInfoDic.Remove(info.socket);
+                        info.socket.Close();
 
-                        Console.WriteLine(string.Format(string.Format("{0}已离线", ip)));
+                        Print(string.Format(string.Format("客户端{0}已离线", ip)), ConsoleColor.Red);
 
                         return;
                     }
 
                     string receiveStr = System.Text.Encoding.Default.GetString(readBuffer, 0, count);
 
-                    OnReceiveMsg(info.Value, receiveStr);
+                    OnReceiveMsg(info, receiveStr);
                 }
             }
         }
@@ -167,27 +191,23 @@ namespace CardExchangeServer {
         void UpdateClientLifeTime() {
             List<Socket> itemToRemove = new List<Socket>();
 
-            foreach (var item in clientInfoDic) {
-                ClientInfo info = item.Value;
+            foreach (var info in clientInfoDic.Values) {
                 info.lifeTime--;
-
-                Console.WriteLine(info.lifeTime);
 
                 //移除
                 if (info.lifeTime <= 0) {
-                    itemToRemove.Add(item.Key);
+                    itemToRemove.Add(info.socket);
                 }
             }
 
             foreach (var item in itemToRemove) {
+                Print(string.Format("客户端{0}超过时限已被移除", GetIP(item)), ConsoleColor.Red);
                 RemoveClient(clientInfoDic[item]);
             }
         }
 
         //移除客户端
         void RemoveClient(ClientInfo info) {
-            Console.WriteLine(string.Format("客户端{0}超过时限已被移除", GetIP(info.socket)));
-
             info.socket.Close();
             clientInfoDic.Remove(info.socket);
         }
@@ -198,6 +218,8 @@ namespace CardExchangeServer {
 
         //当接收客户端
         void OnReceiveClient(Socket client) {
+            Print(string.Format(string.Format("客户端{0}已加入", GetIP(client))), ConsoleColor.Blue);
+
             //新增客户信息
             ClientInfo info = new ClientInfo() { socket = client, lifeTime = DefaultLifetime };
             clientInfoDic.Add(client, info);
@@ -207,39 +229,45 @@ namespace CardExchangeServer {
         void OnReceiveMsg(ClientInfo info, string msg) {
             //转换为客户端数据
             ClientData data = JsonConvert.DeserializeObject<ClientData>(msg);
-            Console.WriteLine(data.time);
 
             //加入客户端列表
-            clientInfoDic[info.socket].data = data;
+            info.data = data;
+
+            string clientData = "";
 
             //遍历客户端列表，找出位置接近的
-            Console.WriteLine("---开始遍历客户端列表---");
-            foreach (var item in clientInfoDic) {
+            foreach (var i in clientInfoDic.Values) {
                 //跳过自己
-                if (item.Value == info) {
+                if (i == info) {
                     continue;
                 }
 
-                if (item.Value.data == null)
+                if (i.data == null)
                     continue;
 
-                float distance = GetDistance(data.loc, item.Value.data.loc);
+                float distance = GetDistance(data.loc, i.data.loc);
+
+                info.lifeTime = DefaultLifetime;
 
                 //比较位置在50米内
-                if (distance < 200) {
-                    Console.WriteLine(string.Format("{0} 距离{1}米", GetIP(item.Key), distance));
+                if (distance < 500) {
+                    //Print(string.Format("{0} 距离{1}米", GetIP(i.socket), distance));
 
                     //重置剩余时间
-                    item.Value.lifeTime = DefaultLifetime;
+                    i.lifeTime = DefaultLifetime;
 
                     //互相发卡
                     ServerData data1 = new ServerData();
-                    data1.client = GetIP(item.Key);
-                    data1.color = item.Value.data.color;
+                    data1.client = GetIP(i.socket);
+                    data1.color = i.data.color;
                     data1.distance = distance;
-                    data1.msg = item.Value.data.msg;
+                    data1.msg = i.data.msg;
                     string s = JsonConvert.SerializeObject(data1);
-                    Send(info.socket, s);
+                    if(clientData != "") {
+                        clientData += "|";
+                    }
+                    clientData += s;
+                    //Send(info.socket, s);
 
                     ServerData data2 = new ServerData();
                     data2.client = GetIP(info.socket);
@@ -247,9 +275,11 @@ namespace CardExchangeServer {
                     data2.distance = distance;
                     data2.msg = info.data.msg;
                     string s2 = JsonConvert.SerializeObject(data2);
-                    Send(item.Key, s2);
+                    Send(i.socket, s2);
                 }
             }
+
+            Send(info.socket, clientData);
         }
 
         //发送消息
@@ -335,6 +365,16 @@ namespace CardExchangeServer {
             var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
             var d = R * c;
             return (float)d * 1000;
+        }
+
+        struct Msg {
+            public string msg;
+            public ConsoleColor color;
+        }
+
+        List<Msg> stringList = new List<Msg>();
+        void Print(object obj, ConsoleColor color = ConsoleColor.White) {
+            stringList.Add(new Msg { msg = obj.ToString(), color = color });
         }
     }
 }
